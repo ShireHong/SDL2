da0c73d5781c2d17853aac1405b8672967949673
ShireHong <shirehong@foxmail.com>
2022-06-26 00:08:32 +0800

    Use libtmx to load tmx format tileset

---
+++ libtmx/build/CMakeFiles/libtmx.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/git_hub/SDL2/libtmx/src/tmx.c" "D:/git_hub/SDL2/libtmx/build/CMakeFiles/libtmx.dir/src/tmx.c.obj"
+  "D:/git_hub/SDL2/libtmx/src/tmx_err.c" "D:/git_hub/SDL2/libtmx/build/CMakeFiles/libtmx.dir/src/tmx_err.c.obj"
+  "D:/git_hub/SDL2/libtmx/src/tmx_hash.c" "D:/git_hub/SDL2/libtmx/build/CMakeFiles/libtmx.dir/src/tmx_hash.c.obj"
+  "D:/git_hub/SDL2/libtmx/src/tmx_mem.c" "D:/git_hub/SDL2/libtmx/build/CMakeFiles/libtmx.dir/src/tmx_mem.c.obj"
+  "D:/git_hub/SDL2/libtmx/src/tmx_utils.c" "D:/git_hub/SDL2/libtmx/build/CMakeFiles/libtmx.dir/src/tmx_utils.c.obj"
+  "D:/git_hub/SDL2/libtmx/src/tmx_xml.c" "D:/git_hub/SDL2/libtmx/build/CMakeFiles/libtmx.dir/src/tmx_xml.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "WANT_ZLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/MinGW/include/SDL2"
+  "../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/git_hub/SDL2/libtmx/src/main.cpp" "D:/git_hub/SDL2/libtmx/build/CMakeFiles/libtmx.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WANT_ZLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/MinGW/include/SDL2"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ libtmx/build/CMakeFiles/libtmx.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libtmx.dir/src/main.cpp.obj"
+  "CMakeFiles/libtmx.dir/src/tmx.c.obj"
+  "CMakeFiles/libtmx.dir/src/tmx_err.c.obj"
+  "CMakeFiles/libtmx.dir/src/tmx_hash.c.obj"
+  "CMakeFiles/libtmx.dir/src/tmx_mem.c.obj"
+  "CMakeFiles/libtmx.dir/src/tmx_utils.c.obj"
+  "CMakeFiles/libtmx.dir/src/tmx_xml.c.obj"
+  "liblibtmx.dll.a"
+  "libtmx.exe"
+  "libtmx.exe.manifest"
+  "libtmx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/libtmx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
